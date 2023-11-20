e04c5797ce8f32d4419e0ab654a341ccd7e49275
Spoorthi Shet <121347112+Spoorthishet2003@users.noreply.github.com>
2023-11-20 22:18:52 +0530

    Create Fibonacci Sequence.py

---
+++ Fibonacci Sequence.py
@@ -0,0 +1,16 @@
+//# A recursive implementation to calculate terms in the Fibonacci sequence. 
+   
+def fn(n):
+    if n <= 2:
+        return n - 1
+    else:
+        return fn(n-1) + fn(n-2)
+
+try:
+    num = int(input("Enter a number : "))
+    if num > 0:
+        print(f' fn({num}) = {fn(num)}')
+    else:
+        print("Input should be greater than 0")
+except ValueError:
+    print("Try with numeric value")
