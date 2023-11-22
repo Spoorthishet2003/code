634cee20a919446ca5ba398784124014c7872d61
Spoorthi Shet <121347112+Spoorthishet2003@users.noreply.github.com>
2023-11-22 21:47:11 +0530

    Update Search file and text.py

--- Search file and text.py
+++ Search file and text.py
@@ -9,6 +9,7 @@ email_regex = re.compile(r'[A-Za-z0-9._]+@[A-Za-z0-9]+\.[A-Z|a-z]{2,}')
 
 # Open the file for reading
 with open('example.txt', 'r') as f:
+    
     # Loop through each line in the file
     for line in f:
        
