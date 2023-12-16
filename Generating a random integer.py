1979b1ab78e10ad8b2380dea3502b7a27276a015
Spoorthi Shet <121347112+Spoorthishet2003@users.noreply.github.com>
2023-12-16 22:49:44 +0530

    Create Generating a random integer.py

---
+++ Generating a random integer.py
@@ -0,0 +1,12 @@
+//#Generating a random integer between 0 to 50 using the randint() function
+
+# Importing the random module  
+import random  
+  
+# Using randint function from the random module  
+n = random.randint(0, 50)  
+  
+# Printing Results  
+print("Random Number between 0 to 50:", n)  
+print("Random Number between 0 to 50:", random.randint(0, 50))  
+print("Random Number between 0 to 50:", random.randint(0, 50))  
