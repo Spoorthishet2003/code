6aebe2a2254353b941451979a1592b6e33a41e22
Spoorthi Shet <121347112+Spoorthishet2003@users.noreply.github.com>
2023-12-16 22:45:32 +0530

    Create Display the calendar.py

---
+++ Display the calendar.py
@@ -0,0 +1,9 @@
+//Display the calendar of any month of any year by importing the calendar module.
+
+# First import the calendar module  
+import calendar  
+# ask of month and year  
+yy = int(input("Enter year: "))  
+mm = int(input("Enter month: "))  
+# display the calendar  
+print(calendar.month(yy,mm))
