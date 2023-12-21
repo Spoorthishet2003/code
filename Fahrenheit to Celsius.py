b492408c48dfdc85426223da2bacf2a26d825e81
Spoorthi Shet <121347112+Spoorthishet2003@users.noreply.github.com>
2023-12-21 22:43:19 +0530

    Create Fahrenheit to Celsius.py

---
+++ Fahrenheit to Celsius.py
@@ -0,0 +1,18 @@
+
+print("Enter Temperature in Fahrenheit: ")
+
+fahren = float(input())
+
+celi = (fahren-32)/1.8
+
+print("\nEquivalent Temperature in Celsius: ", celi)
+
+Output:
+
+Enter Temperature in Fahrenheit:
+
+98
+
+Equivalent Temperature in Celsius
+
+36.6666666666
